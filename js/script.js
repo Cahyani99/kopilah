27c111504c4c85c3fb10d2643ac43c11777eb275
Bela Cahyani <ciciymelaniy99@gmail.com>
2025-06-18 20:48:13 +0700

    tombol pencarian

--- js/script.js
+++ js/script.js
@@ -15,10 +15,18 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+//Toggle class active untuk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
+
 // klik di luar elemen
 
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -28,4 +36,32 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchform.contains(e.target)) {
     searchform.classList.remove("active");
   }
+  if (!sc.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
+  }
 });
+
+//modal box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+//klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
