0e2bdc3d1a84c9abb3d5c75ffd36764d3a60f3be
Bela Cahyani <ciciymelaniy99@gmail.com>
2025-06-19 10:31:10 +0700

    menu sidebar

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-// ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+const hamburger = document.querySelector("#hamburger-menu");
+
+hamburger.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-//Toggle class active untuk search form
+// Toggle class active untuk search form
 const searchform = document.querySelector(".search-form");
 const searchbox = document.querySelector("#search-box");
 
 document.querySelector("#search-button").onclick = (e) => {
   searchform.classList.toggle("active");
-  searchbox.focus();
+  searchbox?.focus();
   e.preventDefault();
 };
 
-//Toggle class active untuk shopping cart
+// Toggle class active untuk shopping cart
 const shoppingcart = document.querySelector(".shopping-cart");
 document.querySelector("#shopping-cart-button").onclick = (e) => {
   shoppingcart.classList.toggle("active");
   e.preventDefault();
 };
 
-// klik di luar elemen
-
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-const sc = document.querySelector("#shopping-cart-button");
-
+// Klik di luar elemen untuk menutup menu
 document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 
+  const sb = document.querySelector("#search-button");
   if (!sb.contains(e.target) && !searchform.contains(e.target)) {
     searchform.classList.remove("active");
   }
+
+  const sc = document.querySelector("#shopping-cart-button");
   if (!sc.contains(e.target) && !shoppingcart.contains(e.target)) {
     shoppingcart.classList.remove("active");
   }
 });
 
-//modal box
-
+// Modal Box
 const itemDetailModal = document.querySelector("#item-detail-modal");
 const itemDetailButtons = document.querySelectorAll(".item-detail-button");
 
@@ -53,13 +51,11 @@ itemDetailButtons.forEach((btn) => {
   };
 });
 
-// klik tombol close modal
 document.querySelector(".modal .close-icon").onclick = (e) => {
   itemDetailModal.style.display = "none";
   e.preventDefault();
 };
 
-//klik di luar modal
 window.onclick = (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = "none";
